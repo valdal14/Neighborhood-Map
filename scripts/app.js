d7c9f8472692518601aae0e27ae67b1c7c2a1593
valdal14 <valdal14@gmail.com>
2017-10-01 11:19:45 +0200

    Final Commit before submission

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,8 @@
-/*jshint strict:false */ 
+/*jshint strict:false */
 /* globals google, ko, $ */
-
 let map;
 let marker;
-let infowindow; 
-let token;
+let infowindow;
 
 /*
  * The array of locations in a client-server application would be the
@@ -49,13 +47,12 @@ class Map {
         this.API_KEY = 'AIzaSyDNUuAV_u1BPgmWZEd4QqgpKXhluxYl3bw';
         this.clientId = "EYM4ZVOPQUCPFYGT233NJ2DV3FRPI3UBC3CNDF331IA4V2WJ";
         this.clientSecret = "EEVZLIERJ4VXZPKTU2LFLMFESPWQWOW3BZUCWCJJVJ2KRCKG";
-        this.token = "";
         this.prefix = "";
         this.size = "";
         this.suffix = "";
         this.width = "";
         this.height = "";
-        this.imageUrl = "";
+        this.pictures = [];
     }
 
     /*
@@ -64,22 +61,16 @@ class Map {
      * arg: takes a user location info array and initialize an array of markers
      */
     createMarkers(clientLocations) {
-        clientLocations.forEach((location) => {
-            fetch(`https://maps.google.com/maps/api/geocode/json?address=${location.title}&key=${this.API_KEY}`, {
-                method: 'post'
-            }).then((response) => {
-                return response.json();
-            }).then((data) => {
-                
-                console.log(data.results[0].geometry.location.lat);
-                console.log(data.results[0].geometry.location.lng);
-                console.log(location.description);
+            clientLocations.forEach((location) => {
+                fetch(`https://maps.google.com/maps/api/geocode/json?address=${location.title}&key=${this.API_KEY}`, {
+                    method: 'post'
+                }).then((response) => {
+                    return response.json();
+                }).then((data) => {
 
+                    //get the venue id
+                    this.getVenue(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng, location.description);
 
-                //get the venue id
-                this.getVenue(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng, location.description);
-
-                setTimeout(()=>{
                     // build a Google Map marker
                     marker = new google.maps.Marker({
                         position: data.results[0].geometry.location,
@@ -90,22 +81,29 @@ class Map {
                         description: location.description
                     });
 
-                    this.addEventToMarkers(marker);
-
                     //fill array of markers with locations data, description and icon image
                     this.markers.push(marker);
 
-                }, 3000);
-                
-
-
-            }).catch((error) => {
-                $('#errorModal').modal('show');
-                $('.errors').text(error);
+                }).catch((error) => {
+                    this.displayErrors(error);
+                });
             });
-        });
 
-    } // end of getMarkers
+            setTimeout(() => {
+                let counter = 0;
+                if (this.pictures.length === this.markers.length) {
+                    this.markers.forEach((marker) => {
+                        this.addEventToMarkers(marker, this.pictures[counter]);
+                        counter += 1;
+                    })
+                }
+
+                //remove the loading modal
+                $('#errorModal').modal('hide');
+
+            }, 5000);
+
+        } // end of getMarkers
 
 
     /*
@@ -114,18 +112,16 @@ class Map {
      * Gets an a marker, add a click event to it and return a marker with a click event attached
      * and information about the location in the infoWindow
      */
-    addEventToMarkers(marker){
-        
+    addEventToMarkers(marker, picture) {
         google.maps.event.addListener(marker, 'click', (function(marker) {
-            return ()=> {
+            return () => {
                 infowindow.setContent(`<h3>${marker.title}</h3>
-                <img width='250px' height='250px' src='${classMap.imageUrl}' alt="${marker.description}">                
+                <img width='250px' height='250px' src='${picture}' alt="${marker.description}">                
                 <p>${marker.description}</p>`);
                 infowindow.open(map, marker);
                 toggleBounce(marker);
             };
         })(marker));
-       
     }
 
     /*
@@ -133,17 +129,19 @@ class Map {
      *
      * Gets an array of markers and return an array with title location strings
      */
-    listLocation(arrayOfMarker){
-        let listLocations = [];
+    listLocation(arrayOfMarker) {
+            let listLocations = [];
 
-        arrayOfMarker.forEach((marker)=>{
-            if(marker.visible){
-                listLocations.push({title: marker.title});
-            }
-        });
+            arrayOfMarker.forEach((marker) => {
+                if (marker.visible) {
+                    listLocations.push({
+                        title: marker.title
+                    });
+                }
+            });
 
-        return listLocations;
-    }// end 
+            return listLocations;
+        } // end 
 
     /*
      * getVenue(latidute, longitude, query)
@@ -151,52 +149,73 @@ class Map {
      * Takes latidute, longitude from markers locations and query from
      * a marker's description and retrieve the venue id
      */
-    getVenue(latidute, longitude, query){
-        fetch(`https://api.foursquare.com/v2/venues/search?ll=${latidute},${longitude}&client_id=${this.clientId}&client_secret=${this.clientSecret}&query=${query}&v=20170929`)
-        .then((response)=>{
-            return response.json();
-        }).then((venue)=>{
-            //get picture data
-            this.getVenuePicture(venue.response.venues[0].id);
-            
-        }).catch((error)=>{
-            $('#errorModal').modal('show');
-            $('.errors').text(error);
-        });
-    }// end
+    getVenue(latidute, longitude, query) {
+            fetch(`https://api.foursquare.com/v2/venues/search?ll=${latidute},${longitude}&client_id=${this.clientId}&client_secret=${this.clientSecret}&query=${query}&v=20170929`)
+                .then((response) => {
+                    return response.json();
+                }).then((venue) => {
+                    //get picture data
+                    this.getVenuePicture(venue.response.venues[0].id);
+
+                }).catch((error) => {
+                    this.displayErrors(error);
+                });
+        } // end
 
     /*
      * getVenuePicture(venueId)
      *
-     * Takes a venue id from a location and 
+     * Takes a venue id from a location and
      */
-    getVenuePicture(venueId){
+    getVenuePicture(venueId) {
         fetch(`https://api.foursquare.com/v2/venues/${venueId}/photos?client_id=${this.clientId}&client_secret=${this.clientSecret}&v=20170929`)
-        .then((response)=>{
-            return response.json();
-        }).then((photo)=>{
-            this.prefix = photo.response.photos.items[0].prefix;
-            this.suffix = photo.response.photos.items[0].suffix;
-            this.width = photo.response.photos.items[0].width;
-            this.height = photo.response.photos.items[0].height;
-            this.size = `${this.width}x${this.height}`;
-            
-            this.imageUrl = this.generatePictureUrl();
+            .then((response) => {
+                return response.json();
+            }).then((photo) => {
+                this.prefix = photo.response.photos.items[0].prefix;
+                this.suffix = photo.response.photos.items[0].suffix;
+                this.width = photo.response.photos.items[0].width;
+                this.height = photo.response.photos.items[0].height;
+                this.size = `${this.width}x${this.height}`;
 
-        }).catch((error)=>{
-            $('#errorModal').modal('show');
-            $('.errors').text(error);
-        });
+                this.pictures.push(this.generatePictureUrl());
+
+            }).catch((error) => {
+                this.displayErrors(error);
+            });
     }
 
     /*
      * getVenuePicture(venueId)
      *
-     * Takes a venue id from a location and 
+     * Takes a venue id from a location and
      */
-    generatePictureUrl(){
-        console.log(`${this.prefix}${this.size}${this.suffix}`);
+    generatePictureUrl() {
         return `${this.prefix}${this.size}${this.suffix}`
+    }
+
+    /*
+     * loading()
+     *
+     * Starts the modal and tells the user to wait
+     */
+    loading(message) {
+        $('.btn').hide();
+        $('.modal-title').text("Welcome to Neighborhood-Map");
+        $('#errorModal').modal('show');
+        $('.errors').text(message);
+    }
+
+    /*
+     * displayErrors(error)
+     *
+     * Starts the modal and tells the user about the error
+     */
+    displayErrors(error) {
+        $('.btn').show();
+        $('.modal-title').text("Opppssss.... There was an error :(");
+        $('#errorModal').modal('show');
+        $('.errors').text(error);
     }
 
 } // end of Map class
@@ -247,13 +266,18 @@ let ViewModel = function() {
 
     // binding the img of the hamburger menu on click event
     this.openCloseMenu = function() {
-            this.openMenu() === false ? this.openMenu(true) : this.openMenu(false);
+        this.openMenu() === false ? this.openMenu(true) : this.openMenu(false);
     }; // end
 
     //handle the click event in the list of locations
     this.listClick = function() {
+        let index = 0;
         classMap.markers.forEach((marker) => {
             if (this.title === marker.title) {
+                index = classMap.markers.indexOf(marker);
+                infowindow.setContent(`<h3>${marker.title}</h3>
+                <img width='250px' height='250px' src='${classMap.pictures[index]}' alt="${marker.description}">                
+                <p>${marker.description}</p>`);
                 infowindow.open(map, marker);
                 toggleBounce(marker);
             }
@@ -264,20 +288,17 @@ let ViewModel = function() {
     this.filterUserText = function() {
 
         if (this.usersearch() === "") {
-
-            classMap.markers.forEach((marker)=>{
+            classMap.markers.forEach((marker) => {
                 marker.setVisible(true);
             });
-            
+
             this.userLocation(classMap.listLocation(classMap.markers));
         }
 
         if (this.usersearch() !== "") {
-
             classMap.markers.forEach((marker) => {
                 this.usersearch() === marker.title || this.usersearch() === marker.description ? marker.setVisible(true) : marker.setVisible(false);
             });
-
             this.userLocation(classMap.listLocation(classMap.markers));
         }
 
@@ -293,6 +314,7 @@ let ViewModel = function() {
 
 //instance of the Map class
 const classMap = new Map(locations);
+classMap.loading("Application is loading, please wait... :)");
 
 //Knockoutjs binding
 ko.applyBindings(new ViewModel());
